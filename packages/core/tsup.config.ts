8b09762e3da68d767fc05141294c5a5a16673ba6
Stijn Van Hulle <stijn@stijnvanhulle.be>
2023-01-08 18:58:59 +0100

    fix: build for browser

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -1,28 +1,41 @@
 import { defineConfig } from 'tsup'
 
-export default defineConfig(({ target }) => ({
-  entry: ['src/index.ts'],
-  treeshake: true,
-  sourcemap: true,
-  minify: false,
-  splitting: false,
-  clean: true,
-  dts: true,
-  /**
-   * @link https://stackoverflow.com/questions/31931614/require-is-not-defined-node-js
-   */
-  banner: {
-    js: target==="es5"? undefined: "import { createRequire } from 'module';const require = createRequire(import.meta.url);",
+export default defineConfig([
+  {
+    entry: ['src/index.ts'],
+    treeshake: true,
+    sourcemap: true,
+    minify: false,
+    splitting: false,
+    clean: true,
+    dts: true,
+    /**
+     * @link https://stackoverflow.com/questions/31931614/require-is-not-defined-node-js
+     */
+    banner: {
+      js: "import { createRequire } from 'module';const require = createRequire(import.meta.url);",
+    },
+    shims: true,
+    format: ['cjs', 'esm'],
   },
-  esbuildOptions: (options) => {
-    if (target === 'es5') {
+  {
+    entry: ['src/index.ts'],
+    outDir: 'dist/browser',
+    treeshake: true,
+    sourcemap: true,
+    minify: false,
+    splitting: false,
+    clean: true,
+    dts: false,
+    esbuildOptions: (options) => {
       options.alias = {
         fs: 'memfs',
+        events: 'events',
         path: 'path-browserify',
         crypto: 'crypto-browserify',
       }
-    }
+    },
+    shims: true,
+    format: ['iife'],
   },
-  shims: true,
-  format: target==="es5"?['iife']: ['cjs', 'esm'],
-}))
+])
