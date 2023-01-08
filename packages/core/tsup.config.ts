453696215014dc969cf9cfef1b1072bd034c4629
Stijn Van Hulle <stijn@stijnvanhulle.be>
2023-01-08 19:15:28 +0100

    chore: revert browser module

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -1,41 +1,19 @@
 import { defineConfig } from 'tsup'
 
-export default defineConfig([
-  {
-    entry: ['src/index.ts'],
-    treeshake: true,
-    sourcemap: true,
-    minify: false,
-    splitting: false,
-    clean: true,
-    dts: true,
-    /**
-     * @link https://stackoverflow.com/questions/31931614/require-is-not-defined-node-js
-     */
-    banner: {
-      js: "import { createRequire } from 'module';const require = createRequire(import.meta.url);",
-    },
-    shims: true,
-    format: ['cjs', 'esm'],
+export default defineConfig({
+  entry: ['src/index.ts'],
+  treeshake: true,
+  sourcemap: true,
+  minify: false,
+  splitting: false,
+  clean: true,
+  dts: true,
+  /**
+   * @link https://stackoverflow.com/questions/31931614/require-is-not-defined-node-js
+   */
+  banner: {
+    js: "import { createRequire } from 'module';const require = createRequire(import.meta.url);",
   },
-  {
-    entry: ['src/index.ts'],
-    outDir: 'dist/browser',
-    treeshake: true,
-    sourcemap: true,
-    minify: false,
-    splitting: false,
-    clean: true,
-    dts: false,
-    esbuildOptions: (options) => {
-      options.alias = {
-        fs: 'memfs',
-        events: 'events',
-        path: 'path-browserify',
-        crypto: 'crypto-browserify',
-      }
-    },
-    shims: true,
-    format: ['cjs', 'esm', 'iife'],
-  },
-])
+  shims: true,
+  format: ['cjs', 'esm'],
+})
