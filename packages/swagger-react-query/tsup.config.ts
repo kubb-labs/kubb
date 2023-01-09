26e4130374f440864d937a6890d99f1c545cb639
Stijn Van Hulle <stijn@stijnvanhulle.be>
2023-01-09 13:09:16 +0100

    fix: revert umd creation for packages that uses ts compiler

--- packages/swagger-react-query/tsup.config.ts
+++ packages/swagger-react-query/tsup.config.ts
@@ -1,52 +1,19 @@
 import { defineConfig } from 'tsup'
 
-export default defineConfig([
-  {
-    entry: ['src/index.ts'],
-    treeshake: true,
-    sourcemap: true,
-    minify: false,
-    splitting: false,
-    clean: true,
-    dts: true,
-    /**
-     * @link https://stackoverflow.com/questions/31931614/require-is-not-defined-node-js
-     */
-    banner: {
-      js: "import { createRequire } from 'module';const require = createRequire(import.meta.url);",
-    },
-    shims: true,
-    format: ['cjs', 'esm'],
+export default defineConfig({
+  entry: ['src/index.ts'],
+  treeshake: true,
+  sourcemap: true,
+  minify: false,
+  splitting: false,
+  clean: true,
+  dts: true,
+  /**
+   * @link https://stackoverflow.com/questions/31931614/require-is-not-defined-node-js
+   */
+  banner: {
+    js: "import { createRequire } from 'module';const require = createRequire(import.meta.url);",
   },
-  {
-    entry: ['src/index.ts'],
-    treeshake: true,
-    sourcemap: true,
-    minify: false,
-    splitting: false,
-    clean: false,
-    dts: false,
-    globalName: 'kubbSwaggerReactQuery',
-    platform: 'browser',
-    esbuildOptions: (options) => {
-      options.alias = {
-        fs: 'memfs',
-        'graceful-fs': 'memfs',
-        events: 'eventemitter3',
-        path: 'path-browserify',
-        stream: 'stream-browserify',
-        process: 'process/browser',
-        https: 'https-browserify',
-        http: 'http-browserify',
-        os: 'os-browserify',
-      }
-      options.define = {
-        global: 'globalThis',
-        __filename: JSON.stringify(''),
-        process: JSON.stringify({ env: {} }),
-      }
-    },
-    shims: true,
-    format: ['iife'],
-  },
-])
+  shims: true,
+  format: ['cjs', 'esm'],
+})
