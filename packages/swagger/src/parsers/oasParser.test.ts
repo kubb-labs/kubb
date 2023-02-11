c0131b9c38930080f84452f9e90b9e186fd1cfd3
Stijn Van Hulle <stijn@stijnvanhulle.be>
2023-02-11 20:36:59 +0100

    test: oasParser with Swagger v2 file

--- packages/swagger/src/parsers/oasParser.test.ts
+++ packages/swagger/src/parsers/oasParser.test.ts
@@ -3,10 +3,18 @@ import pathParser from 'path'
 import { oasPathParser } from './oasParser'
 
 describe('oasParser', () => {
-  const path = pathParser.resolve(__dirname, '../../mocks/petStore.yaml')
+  const petStoreV3 = pathParser.resolve(__dirname, '../../mocks/petStore.yaml')
+  const petStoreV2 = pathParser.resolve(__dirname, '../../mocks/petStoreV2.json')
 
   test('check if oas and title is defined based on a Swagger(v3) file', async () => {
-    const oas = await oasPathParser(path)
+    const oas = await oasPathParser(petStoreV3)
+
+    expect(oas).toBeDefined()
+    expect(oas.api?.info.title).toBe('Swagger Petstore')
+  })
+
+  test('check if oas and title is defined based on a Swagger(v2) file', async () => {
+    const oas = await oasPathParser(petStoreV2)
 
     expect(oas).toBeDefined()
     expect(oas.api?.info.title).toBe('Swagger Petstore')
