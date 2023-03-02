518a15f7dc1170a899a69b90ebd82ab4859099eb
Stijn Van Hulle <stijn@stijnvanhulle.be>
2023-03-02 19:36:58 +0100

    chore: add analytics for docs

--- docs/pages/_app.js
+++ docs/pages/_app.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Analytics } from '@vercel/analytics/react'
 import 'nextra-theme-docs/style.css'
 import '../styles.css'
 
 export default function Nextra({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
 }
