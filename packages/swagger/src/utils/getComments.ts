7c38ee020f5b54ff2cfec807315c1450119025d0
Stijn Van Hulle <stijn@stijnvanhulle.be>
2023-04-09 20:40:51 +0200

    fix: use of express style for jsdoc @link

--- packages/swagger/src/utils/getComments.ts
+++ packages/swagger/src/utils/getComments.ts
@@ -1,10 +1,12 @@
+import { Path } from '.'
+
 import type { Operation } from 'oas'
 
 export function getComments(operation: Operation): string[] {
   return [
     operation.getDescription() && `@description ${operation.getDescription()}`,
     operation.getSummary() && `@summary ${operation.getSummary()}`,
-    operation.path && `@link ${operation.path}`,
+    operation.path && `@link ${new Path(operation.path).URL}`,
     operation.isDeprecated() && `@deprecated`,
   ].filter(Boolean) as string[]
 }
