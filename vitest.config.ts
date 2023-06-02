94639af81120371055ce000982b239bdd0416ece
Stijn Van Hulle <stijn@stijnvanhulle.be>
2023-06-02 12:06:53 +0200

    chore: ci update for coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,11 @@
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  test: { globals: true },
+  test: {
+    globals: true,
+    exclude: ['**/node_modules/**', '**/dist/**'],
+    coverage: {
+      exclude: ['**/dist/**'],
+    },
+  },
 })
