a446b188e5001b3597125bcfed61e454e9ed06c0
Stijn Van Hulle <stijn@stijnvanhulle.be>
2023-06-05 22:29:50 +0200

    fix: Path and objectToParameters with paramterName in camelCase

---
+++ packages/swagger/src/utils/Path.test.ts
@@ -0,0 +1,13 @@
+/* eslint-disable no-template-curly-in-string */
+import { Path } from './Path.ts'
+
+describe('Path', () => {
+  const path = new Path('/user/{userID}/monetary-account/{monetary-accountID}/whitelist-sdd/{itemId}')
+  test('if templateStrings returns correct format', async () => {
+    expect(path.template).toBe('`/user/${userId}/monetary-account/${monetaryAccountId}/whitelist-sdd/${itemId}`')
+  })
+
+  test('if URL path returns the correct format', async () => {
+    expect(path.URL).toBe('/user/:userID/monetary-account/:monetary-accountID/whitelist-sdd/:itemId')
+  })
+})
