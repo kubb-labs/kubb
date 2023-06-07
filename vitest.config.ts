3ed48e2610ad59d2c02876f2b777293a41158582
Stijn Van Hulle <stijn@stijnvanhulle.be>
2023-06-07 23:54:05 +0200

    chore: exclude dist coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,19 @@ export default defineConfig({
     globals: true,
     exclude: ['**/node_modules/**', '**/dist/**'],
     coverage: {
-      exclude: ['**/dist/**'],
+      exclude: [
+        '**/dist/**',
+        '**/mocks/**',
+        '**/coverage/**',
+        '**/packages/*/test?(s)/**',
+        '**/*.d.ts',
+        'test?(s)/**',
+        'test?(-*).?(c|m)[jt]s?(x)',
+        '**/*{.,-}{test,spec}.?(c|m)[jt]s?(x)',
+        '**/__tests__/**',
+        '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+        '**/.{eslint,mocha,prettier}rc.{?(c|m)js,yml}',
+      ],
     },
   },
 })
