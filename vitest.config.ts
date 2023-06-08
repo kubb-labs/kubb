f4112efc4abc3a95e50b58a3fa925c7425d911dd
Stijn Van Hulle <stijn@stijnvanhulle.be>
2023-06-08 11:28:12 +0200

    fix: drop of node 16 in favour of node 18 as minimum version

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
     exclude: ['**/node_modules/**', '**/dist/**'],
     coverage: {
       exclude: [
+        '**/plugin.ts', // exclude because we have e2e
         '**/dist/**',
         '**/mocks/**',
         '**/coverage/**',
