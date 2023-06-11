43652dbe25a8d1b2b3590d6fefc5d48123c86f88
Stijn Van Hulle <stijn@stijnvanhulle.be>
2023-06-11 15:29:03 +0200

    chore: eslint update with jsdocs added

--- packages/config/eslint-config/src/index.ts
+++ packages/config/eslint-config/src/index.ts
@@ -25,7 +25,7 @@ const config: Linter.Config = {
   rules,
   overrides: [
     {
-      files: ['examples/**'],
+      files: ['examples/**', 'e2e/**'],
       rules: {
         'import/extensions': [
           'warn',
