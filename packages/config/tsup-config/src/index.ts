565b29a745f49762631020adb24452ede61d5344
Stijn Van Hulle <stijn@stijnvanhulle.be>
2023-06-11 13:22:37 +0200

    chore: use 'tsup' in e2e package.json

--- packages/config/tsup-config/src/index.ts
+++ packages/config/tsup-config/src/index.ts
@@ -42,13 +42,13 @@ export const optionsESM: Options = {
     },
   },
   banner: bannerESM,
-  esbuildOptions: (options) => {
-    options.footer = {
-      // This will ensure we can continue writing this plugin
-      // as a modern ECMA module, while still publishing this as a CommonJS
-      // library with a default export, as that's how ESLint expects plugins to look.
-      // @see https://github.com/evanw/esbuild/issues/1182#issuecomment-1011414271
-      js: 'module.exports = module.exports.default;',
-    }
-  },
+  // esbuildOptions: (options) => {
+  //   options.footer = {
+  //     // This will ensure we can continue writing this plugin
+  //     // as a modern ECMA module, while still publishing this as a CommonJS
+  //     // library with a default export, as that's how ESLint expects plugins to look.
+  //     // @see https://github.com/evanw/esbuild/issues/1182#issuecomment-1011414271
+  //     js: 'module.exports = module.exports.default;',
+  //   }
+  // },
 }
