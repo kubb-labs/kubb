15c0cd34f54629292d30c57e162dad4467f8b9c8
Alfred <b6pzeusbc54tvhw5jgpyw8pwz2x6gs@gmail.com>
2023-10-18 03:39:36 +0900

    fix: docs about wrong Badge usage, wrong import line example

--- packages/swagger-tanstack-query/src/types.ts
+++ packages/swagger-tanstack-query/src/types.ts
@@ -32,7 +32,7 @@ export type Options = {
   client?: string
   /**
    * Path to the client import path that will be used to do the API calls.
-   * It will be used as `import { client } from '{{clientImportPath}}'`.
+   * It will be used as `import client from '${clientImportPath}'`.
    * It allow both relative and absolute path.
    * the path will be applied as is, so relative path shoule be based on the file being generated.
    * @default '@kubb/swagger-client/client'
