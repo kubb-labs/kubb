01eafac764c261b4839efec29180505c92720884
Stijn Van Hulle <stijn@stijnvanhulle.be>
2023-10-27 19:11:22 +0200

    Revert "ci: build script for alpha and canary"
    This reverts commit 3d958ef497091b3af1da73227a9aded2b4ecd27c.

--- examples/react-query-v5/.codesandbox/build.js
+++ examples/react-query-v5/.codesandbox/build.js
@@ -1,18 +1,21 @@
 import fs from 'node:fs'
 
-const tag = process.argv[process.argv.length - 1]
-
-const pkgJsonPaths = ['package.json']
+const pkgJsonPaths = [
+  'package.json',
+]
 try {
   for (const pkgJsonPath of pkgJsonPaths) {
     const pkg = JSON.parse(fs.readFileSync(pkgJsonPath, 'utf-8'))
     const oldVersion = 'workspace:*'
-    const newVersion = ['latest', 'canary', 'alpha'].includes(tag) || pkg.version
+    const newVersion = 'latest'
 
     const content = JSON.stringify(pkg, null, '\t') + '\n'
     const newContent = content
       // @ts-ignore
-      .replaceAll(`"${oldVersion}"`, `"${newVersion}"`)
+      .replaceAll(
+        `"${oldVersion}"`,
+        `"${newVersion}"`,
+      )
 
     fs.writeFileSync(pkgJsonPath, newContent)
   }
