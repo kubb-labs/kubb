8c4756b096458b09f730fb63d9836e51a1a6bd14
Vasilii Mikhailovskii <ti.webdev@gmail.com>
2023-12-01 18:47:16 +0500

    feat: add support unicode to zod regexp pattern

--- packages/swagger-zod/src/generators/ZodGenerator.ts
+++ packages/swagger-zod/src/generators/ZodGenerator.ts
@@ -109,7 +109,7 @@ export class ZodGenerator extends SchemaGenerator<Options, OpenAPIV3.SchemaObjec
           const isStartWithSlash = matches.startsWith('/')
           const isEndWithSlash = matches.endsWith('/')
 
-          const regexp = `new RegExp('${jsStringEscape(matches.slice(isStartWithSlash ? 1 : 0, isEndWithSlash ? -1 : undefined))}')`
+          const regexp = `new RegExp('${jsStringEscape(matches.slice(isStartWithSlash ? 1 : 0, isEndWithSlash ? -1 : undefined))}', 'u')`
 
           validationFunctions.push({ keyword: zodKeywords.matches, args: regexp })
         }
