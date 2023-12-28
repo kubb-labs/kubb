b1837e88dec5ad0eebc467c11a5a9dc1668fcbdb
Stijn Van Hulle <stijn@stijnvanhulle.be>
2023-12-28 21:47:59 +0100

    chore: clean

--- packages/parser/src/print.test.ts
+++ packages/parser/src/print.test.ts
@@ -1,7 +1,3 @@
-import path from 'node:path'
-
-import ts, { createProgram, displayPartsToString } from 'typescript'
-
 import { format } from '../mocks/format.ts'
 import { print } from './print.ts'
 
