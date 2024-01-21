6aa72c009741186ffc3e2a59f8dc4462c9f98a73
Stijn Van Hulle <stijn@stijnvanhulle.be>
2024-01-21 19:33:49 +0100

    chore: prepare unplugin setup

--- examples/react-query-v5/vite.config.ts
+++ examples/react-query-v5/vite.config.ts
@@ -1,7 +1,18 @@
 import react from '@vitejs/plugin-react'
+import kubb from 'unplugin-kubb/vite'
 import { defineConfig } from 'vite'
+import createSwagger from '@kubb/swagger'
+import createSwaggerTanstackQuery from '@kubb/swagger-tanstack-query'
+import createSwaggerTS from '@kubb/swagger-ts'
+
+import { config } from './kubb.config.js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    kubb({
+      config,
+    }),
+  ],
 })
