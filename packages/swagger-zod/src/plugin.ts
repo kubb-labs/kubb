95387b222e55402a134bad80509f67b9a5ff3c55
Stijn Van Hulle <stijn@stijnvanhulle.be>
2024-01-21 18:18:19 +0100

    chore: fix plugin zod

--- packages/swagger-zod/src/plugin.ts
+++ packages/swagger-zod/src/plugin.ts
@@ -54,7 +54,7 @@ export const definePlugin = createPlugin<PluginOptions>((options) => {
       return path.resolve(root, output.path, baseName)
     },
     resolveName(name, type) {
-      const resolvedName = camelCase(name, { suffix: 'schema', isFile: type === 'file' })
+      const resolvedName = camelCase(name, { suffix: type ? 'schema' : undefined, isFile: type === 'file' })
 
       if (type) {
         return transformers?.name?.(resolvedName, type) || resolvedName
