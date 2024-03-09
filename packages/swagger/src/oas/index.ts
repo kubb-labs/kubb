7a1b951bfcc8cfb47c1f771804960be134a1b3b6
Stijn Van Hulle <stijn@stijnvanhulle.be>
2024-03-09 16:38:10 +0100

    chore: fix ts

--- packages/swagger/src/oas/index.ts
+++ packages/swagger/src/oas/index.ts
@@ -1,6 +1,10 @@
+import type * as OasTypes from 'oas/types'
+
 // external packages
 export type { default as Oas } from 'oas'
 export type { Operation } from 'oas/operation'
 export type { HttpMethods as HttpMethod } from 'oas/types'
 export type * as OasTypes from 'oas/types'
 export type { OpenAPIV3, OpenAPIV3_1 } from 'openapi-types'
+
+export type SchemaObject = OasTypes.SchemaObject & { 'x-nullable'?: boolean }
