ad6f50de6406f34a2d8e28a082ed3f4f7c1e9105
Stijn Van Hulle <stijn@stijnvanhulle.be>
2024-03-11 20:52:37 +0100

    chore: set noExternal

--- packages/parser/tsup.config.ts
+++ packages/parser/tsup.config.ts
@@ -1,20 +1,22 @@
-import { optionsCJS, optionsESM } from '@kubb/tsup-config'
+import { optionsCJS, optionsESM } from "@kubb/tsup-config";
 
-import { defineConfig } from 'tsup'
+import { defineConfig } from "tsup";
 
 export default defineConfig([
   {
     ...optionsCJS,
     entry: {
-      index: 'src/index.ts',
-      factory: 'src/factory.ts',
+      index: "src/index.ts",
+      factory: "src/factory.ts",
     },
+    noExternal: [/lodash.tonumber/],
   },
   {
     ...optionsESM,
     entry: {
-      index: 'src/index.ts',
-      factory: 'src/factory.ts',
+      index: "src/index.ts",
+      factory: "src/factory.ts",
     },
+    noExternal: [/lodash.tonumber/],
   },
-])
+]);
