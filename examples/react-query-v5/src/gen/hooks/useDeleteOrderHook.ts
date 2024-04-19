e48b976610c3eb69501b8b1e6e2fd8aadf2bac8a
Stijn Van Hulle <stijn@stijnvanhulle.be>
2024-04-19 23:18:42 +0200

    chore: format

--- examples/react-query-v5/src/gen/hooks/useDeleteOrderHook.ts
+++ examples/react-query-v5/src/gen/hooks/useDeleteOrderHook.ts
@@ -1,49 +1,50 @@
-import client from "@kubb/swagger-client/client";
-import { useMutation } from "@tanstack/react-query";
-import { useInvalidationForMutation } from "../../useInvalidationForMutation";
-import type { DeleteOrderMutationResponse, DeleteOrderPathParams, DeleteOrder400, DeleteOrder404 } from "../models/DeleteOrder";
-import type { UseMutationOptions } from "@tanstack/react-query";
+import client from '@kubb/swagger-client/client'
+import { useMutation } from '@tanstack/react-query'
+import type { UseMutationOptions } from '@tanstack/react-query'
+import { useInvalidationForMutation } from '../../useInvalidationForMutation'
+import type { DeleteOrder400, DeleteOrder404, DeleteOrderMutationResponse, DeleteOrderPathParams } from '../models/DeleteOrder'
 
- type DeleteOrderClient = typeof client<DeleteOrderMutationResponse, DeleteOrder400 | DeleteOrder404, never>;
+type DeleteOrderClient = typeof client<DeleteOrderMutationResponse, DeleteOrder400 | DeleteOrder404, never>
 type DeleteOrder = {
-    data: DeleteOrderMutationResponse;
-    error: DeleteOrder400 | DeleteOrder404;
-    request: never;
-    pathParams: DeleteOrderPathParams;
-    queryParams: never;
-    headerParams: never;
-    response: DeleteOrderMutationResponse;
-    client: {
-        parameters: Partial<Parameters<DeleteOrderClient>[0]>;
-        return: Awaited<ReturnType<DeleteOrderClient>>;
-    };
-};
+  data: DeleteOrderMutationResponse
+  error: DeleteOrder400 | DeleteOrder404
+  request: never
+  pathParams: DeleteOrderPathParams
+  queryParams: never
+  headerParams: never
+  response: DeleteOrderMutationResponse
+  client: {
+    parameters: Partial<Parameters<DeleteOrderClient>[0]>
+    return: Awaited<ReturnType<DeleteOrderClient>>
+  }
+}
 /**
  * @description For valid response try integer IDs with value < 1000. Anything above 1000 or nonintegers will generate API errors
  * @summary Delete purchase order by ID
  * @link /store/order/:orderId
  */
-export function useDeleteOrderHook(orderId: DeleteOrderPathParams["orderId"], options: {
-    mutation?: UseMutationOptions<DeleteOrder["response"], DeleteOrder["error"], DeleteOrder["request"]>;
-    client?: DeleteOrder["client"]["parameters"];
-} = {}) {
-    const { mutation: mutationOptions, client: clientOptions = {} } = options ?? {};
-    const invalidationOnSuccess = useInvalidationForMutation("useDeleteOrderHook");
-    return useMutation({
-        mutationFn: async () => {
-            const res = await client<DeleteOrder["data"], DeleteOrder["error"], DeleteOrder["request"]>({
-                method: "delete",
-                url: `/store/order/${orderId}`,
-                ...clientOptions
-            });
-            return res.data;
-        },
-        onSuccess: (...args) => {
-            if (invalidationOnSuccess)
-                invalidationOnSuccess(...args);
-            if (mutationOptions?.onSuccess)
-                mutationOptions.onSuccess(...args);
-        },
-        ...mutationOptions
-    });
+export function useDeleteOrderHook(
+  orderId: DeleteOrderPathParams['orderId'],
+  options: {
+    mutation?: UseMutationOptions<DeleteOrder['response'], DeleteOrder['error'], DeleteOrder['request']>
+    client?: DeleteOrder['client']['parameters']
+  } = {},
+) {
+  const { mutation: mutationOptions, client: clientOptions = {} } = options ?? {}
+  const invalidationOnSuccess = useInvalidationForMutation('useDeleteOrderHook')
+  return useMutation({
+    mutationFn: async () => {
+      const res = await client<DeleteOrder['data'], DeleteOrder['error'], DeleteOrder['request']>({
+        method: 'delete',
+        url: `/store/order/${orderId}`,
+        ...clientOptions,
+      })
+      return res.data
+    },
+    onSuccess: (...args) => {
+      if (invalidationOnSuccess) invalidationOnSuccess(...args)
+      if (mutationOptions?.onSuccess) mutationOptions.onSuccess(...args)
+    },
+    ...mutationOptions,
+  })
 }
--- examples/vue-query-v5/src/gen/hooks/useDeletePet.ts
+++ examples/vue-query-v5/src/gen/hooks/useDeletePet.ts
@@ -1,46 +1,50 @@
-import client from "@kubb/swagger-client/client";
-import { useMutation } from "@tanstack/vue-query";
-import { unref } from "vue";
-import type { DeletePetMutationResponse, DeletePetPathParams, DeletePetHeaderParams, DeletePet400 } from "../models/DeletePet";
-import type { UseMutationOptions } from "@tanstack/vue-query";
-import type { MaybeRef } from "vue";
+import client from '@kubb/swagger-client/client'
+import { useMutation } from '@tanstack/vue-query'
+import type { UseMutationOptions } from '@tanstack/vue-query'
+import { unref } from 'vue'
+import type { MaybeRef } from 'vue'
+import type { DeletePet400, DeletePetHeaderParams, DeletePetMutationResponse, DeletePetPathParams } from '../models/DeletePet'
 
- type DeletePetClient = typeof client<DeletePetMutationResponse, DeletePet400, never>;
+type DeletePetClient = typeof client<DeletePetMutationResponse, DeletePet400, never>
 type DeletePet = {
-    data: DeletePetMutationResponse;
-    error: DeletePet400;
-    request: never;
-    pathParams: DeletePetPathParams;
-    queryParams: never;
-    headerParams: DeletePetHeaderParams;
-    response: DeletePetMutationResponse;
-    client: {
-        parameters: Partial<Parameters<DeletePetClient>[0]>;
-        return: Awaited<ReturnType<DeletePetClient>>;
-    };
-};
+  data: DeletePetMutationResponse
+  error: DeletePet400
+  request: never
+  pathParams: DeletePetPathParams
+  queryParams: never
+  headerParams: DeletePetHeaderParams
+  response: DeletePetMutationResponse
+  client: {
+    parameters: Partial<Parameters<DeletePetClient>[0]>
+    return: Awaited<ReturnType<DeletePetClient>>
+  }
+}
 /**
  * @description delete a pet
  * @summary Deletes a pet
  * @link /pet/:petId
  */
-export function useDeletePet(refPetId: MaybeRef<DeletePetPathParams["petId"]>, refHeaders?: MaybeRef<DeletePetHeaderParams>, options: {
-    mutation?: UseMutationOptions<DeletePet["response"], DeletePet["error"], void, unknown>;
-    client?: DeletePet["client"]["parameters"];
-} = {}) {
-    const { mutation: mutationOptions, client: clientOptions = {} } = options ?? {};
-    return useMutation({
-        mutationFn: async (data) => {
-            const petId = unref(refPetId);
-            const headers = unref(refHeaders);
-            const res = await client<DeletePet["data"], DeletePet["error"], DeletePet["request"]>({
-                method: "delete",
-                url: `/pet/${petId}`,
-                headers: { ...headers, ...clientOptions.headers },
-                ...clientOptions
-            });
-            return res.data;
-        },
-        ...mutationOptions
-    });
+export function useDeletePet(
+  refPetId: MaybeRef<DeletePetPathParams['petId']>,
+  refHeaders?: MaybeRef<DeletePetHeaderParams>,
+  options: {
+    mutation?: UseMutationOptions<DeletePet['response'], DeletePet['error'], void, unknown>
+    client?: DeletePet['client']['parameters']
+  } = {},
+) {
+  const { mutation: mutationOptions, client: clientOptions = {} } = options ?? {}
+  return useMutation({
+    mutationFn: async (data) => {
+      const petId = unref(refPetId)
+      const headers = unref(refHeaders)
+      const res = await client<DeletePet['data'], DeletePet['error'], DeletePet['request']>({
+        method: 'delete',
+        url: `/pet/${petId}`,
+        headers: { ...headers, ...clientOptions.headers },
+        ...clientOptions,
+      })
+      return res.data
+    },
+    ...mutationOptions,
+  })
 }
