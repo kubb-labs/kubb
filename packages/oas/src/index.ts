1661356e1d24778a0e569b5ee7efcbc0eec7032d
Stijn Van Hulle <stijn@stijnvanhulle.be>
2024-04-25 20:44:54 +0200

    chore: tsconfig

--- packages/oas/src/index.ts
+++ packages/oas/src/index.ts
@@ -1,3 +1,5 @@
+import './typings.d.ts'
+
 export * from './types.ts'
 export { findSchemaDefinition, matchesMimeType } from 'oas/utils'
 export { isRequired, isReference, isParameterObject, isOpenApiV3_1Document } from './utils.ts'
