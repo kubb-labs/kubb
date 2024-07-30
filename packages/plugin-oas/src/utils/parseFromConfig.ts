cb0b7485646a9448cbdc55b77e2c02c5b7a4900c
rajp33 <44758822+rajp33@users.noreply.github.com>
2024-07-30 15:32:10 -0400

    Allow Overriding of OAS class (to support modifications when getting parameters/request/schemas) (#1092)
    * feat: pass in oasClass with default to allow override
    
    * chore: changeset

--- packages/plugin-oas/src/utils/parseFromConfig.ts
+++ packages/plugin-oas/src/utils/parseFromConfig.ts
@@ -5,32 +5,32 @@ import { URLPath } from '@kubb/core/utils'
 import { type FormatOptions, parse } from '@kubb/oas/parser'
 
 import type { Config } from '@kubb/core'
-import type { Oas, OasTypes } from '@kubb/oas'
+import { Oas, type OasTypes } from '@kubb/oas'
 import yaml from '@stoplight/yaml'
 
-export function parseFromConfig(config: Config, options: FormatOptions = {}): Promise<Oas> {
+export function parseFromConfig(config: Config, options: FormatOptions = {}, oasClass: typeof Oas = Oas): Promise<Oas> {
   if ('data' in config.input) {
     if (typeof config.input.data === 'object') {
       const api: OasTypes.OASDocument = JSON.parse(JSON.stringify(config.input.data)) as OasTypes.OASDocument
-      return parse(api, options)
+      return parse(api, options, oasClass)
     }
 
     try {
       const api: string = yaml.parse(config.input.data as string) as string
 
-      return parse(api, options)
+      return parse(api, options, oasClass)
     } catch (e) {
       /* empty */
     }
 
     const api: OasTypes.OASDocument = JSON.parse(JSON.stringify(config.input.data)) as OasTypes.OASDocument
 
-    return parse(api, options)
+    return parse(api, options, oasClass)
   }
 
   if (new URLPath(config.input.path).isURL) {
-    return parse(config.input.path, options)
+    return parse(config.input.path, options, oasClass)
   }
 
-  return parse(resolve(config.root, config.input.path), options)
+  return parse(resolve(config.root, config.input.path), options, oasClass)
 }
