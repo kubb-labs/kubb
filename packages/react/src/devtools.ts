179184c16d6b1d22ccfdd94f03c27271d3e52e2f
Stijn Van Hulle <stijn@stijnvanhulle.be>
2024-08-31 00:41:36 +0200

    chore: devtools auto open with execa

--- packages/react/src/devtools.ts
+++ packages/react/src/devtools.ts
@@ -1,4 +1,15 @@
+import { execa } from 'execa'
 // @ts-ignore
 import devtools from 'react-devtools-core'
 
-devtools.connectToDevTools()
+function openDevtools() {
+  console.log("Opening devtools")
+  execa({preferLocal: true, detached: true,  })`npx react-devtools`;
+  
+  console.log("Connecting devtools")
+  devtools.connectToDevTools()
+}
+
+openDevtools()
+
+
