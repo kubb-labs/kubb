0bfedae4dc4f3fd6d21d312a03227f579b562541
Stijn Van Hulle <stijn@stijnvanhulle.be>
2024-09-01 20:18:42 +0200

    chore: Machines API has an invalid swagger file

--- e2e/kubb.config.js
+++ e2e/kubb.config.js
@@ -11,7 +11,7 @@ import { pluginZodios } from '@kubb/swagger-zodios'
 
 const schemas = [
   ['petStoreV3', 'https://petstore3.swagger.io/api/v3/openapi.json'],
-  ['Machines API', 'https://docs.machines.dev/spec/openapi3.json'],
+  // ['Machines API', 'https://docs.machines.dev/spec/openapi3.json'], // invalid
   ['optionalParameters', './schemas/optionalParameters.json'],
   ['allOf', './schemas/allOf.json'],
   ['anyOf', './schemas/anyOf.json'],
