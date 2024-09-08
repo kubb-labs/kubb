176e098a1a2ac4d5d5ae384b648d1cab8d108060
Stijn Van Hulle <stijn@stijnvanhulle.be>
2024-09-08 02:58:52 +0200

    chore: cleanup faker

--- packages/plugin-client/src/generators/__snapshots__/deletePet.ts
+++ packages/plugin-client/src/generators/__snapshots__/deletePet.ts
@@ -5,6 +5,5 @@
  */
 export async function deletePet(petId: DeletePet['petId'], headers?: DeletePet, config: Partial<RequestConfig> = {}) {
   const res = await client<DeletePet>({ method: 'delete', url: `/pet/${petId}`, headers: { ...headers, ...config.headers }, ...config })
-
   return res.data
 }
