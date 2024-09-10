12b0f41da380937d15358ec3fc0f838f5ece7e8c
Stijn Van Hulle <stijn@stijnvanhulle.be>
2024-09-11 00:22:24 +0200

    docs: full build

--- packages/plugin-oas/src/utils/index.ts
+++ packages/plugin-oas/src/utils/index.ts
@@ -3,7 +3,6 @@ export { getASTParams, getPathParams } from './getParams.ts'
 export { getSchemaFactory } from './getSchemaFactory.ts'
 export type { GetSchemasProps } from './getSchemas.ts'
 export { getSchemas } from './getSchemas.ts'
-export { refsSorter } from './refSorter.ts'
 export { parseFromConfig } from './parseFromConfig.ts'
 
 export { isOptional } from '@kubb/oas'
