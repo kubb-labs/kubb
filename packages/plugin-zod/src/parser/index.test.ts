e8c2a7f3ef6c41e74acaa6ba6bf7b78a9de00769
Christoffer Nørbjerg <ChilloManiac@users.noreply.github.com>
2024-10-23 19:36:18 +0200

    feat: siblings in parser-zod for better AST manipulation (#1342)
    Co-authored-by: Christoffer N <Christoffer.norbjerg@lego.com>
    Co-authored-by: Stijn Van Hulle <stijn@stijnvanhulle.be>

--- packages/plugin-zod/src/parser/index.test.ts
+++ packages/plugin-zod/src/parser/index.test.ts
@@ -4,7 +4,7 @@ import * as parserZod from './index.ts'
 
 describe('zod parse', () => {
   test.each(schemas.basic)('$name', ({ name, schema }) => {
-    const text = parserZod.parse(undefined, schema, { name })
+    const text = parserZod.parse({ parent: undefined, current: schema, siblings: [schema] }, { name })
     expect(text).toMatchSnapshot()
   })
 })
