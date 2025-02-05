0bb0c0347e08c61b3b1b0433964241e89cabfdaa
Stijn Van Hulle <stijn@stijnvanhulle.be>
2025-02-05 22:43:35 +0100

    chore: revert export

--- packages/plugin-oas/src/utils/index.ts
+++ packages/plugin-oas/src/utils/index.ts
@@ -1,6 +1,7 @@
 export { getComments } from './getComments.ts'
 export { getASTParams, getPathParams } from './getParams.ts'
 export { getSchemaFactory } from './getSchemaFactory.ts'
+export { getSchemas } from './getSchemas.ts'
 export { parseFromConfig } from './parseFromConfig.ts'
 export { getBanner } from './getBanner.ts'
 export { getFooter } from './getFooter.ts'
