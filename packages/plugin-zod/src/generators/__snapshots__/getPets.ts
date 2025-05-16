57c729bd61315a6a84b72fb23ae4989b80bf068c
Stefan du Fresne <stefan.dufresne@ovo.com>
2025-05-16 13:56:26 +0100

    Query object is now longer optional if any parameter has default
    If a parameter has a default then there will always be a value, which means the object will always exist, which means from a type perspective it can never be optional.

--- packages/plugin-zod/src/generators/__snapshots__/getPets.ts
+++ packages/plugin-zod/src/generators/__snapshots__/getPets.ts
@@ -4,12 +4,10 @@
  */
 import { z } from 'zod'
 
-export const listPetsQueryParams = z
-  .object({
-    limit: z.string().describe('How many items to return at one time (max 100)').optional(),
-    offset: z.coerce.number().int().default(0),
-  })
-  .optional()
+export const listPetsQueryParams = z.object({
+  limit: z.string().describe('How many items to return at one time (max 100)').optional(),
+  offset: z.coerce.number().int().default(0),
+})
 
 /**
  * @description A paged array of pets
--- packages/plugin-zod/src/generators/__snapshots__/queryAllDefaulted_wrapOutput.ts
+++ packages/plugin-zod/src/generators/__snapshots__/queryAllDefaulted_wrapOutput.ts
@@ -4,11 +4,10 @@
  */
 import { z } from '@hono/zod-openapi'
 
-export const getThingsQueryParams = z
-  .object({
-    limit: z.coerce.number().int().min(1).max(100).default(100).describe('Maximum number of things to return'),
-    skip: z.coerce.number().int().min(0).default(0).describe('Number of things to skip'),
-  })
+export const getThingsQueryParams = z.object({
+  limit: z.coerce.number().int().min(1).max(100).default(100).describe('Maximum number of things to return'),
+  skip: z.coerce.number().int().min(0).default(0).describe('Number of things to skip'),
+})
 
 /**
  * @description A list of things
