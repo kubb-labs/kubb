7e37060f88d0fd2ab10b672232cfcdfcbdf00da1
Stijn Van Hulle <stijn@stijnvanhulle.be>
2025-08-31 19:56:37 +0200

    chore: remove machines openapi

--- e2e/kubb.config.js
+++ e2e/kubb.config.js
@@ -15,7 +15,7 @@ import { pluginZod } from '@kubb/plugin-zod'
 const schemas = [
   // { name: 'test', path: './schemas/test.json' },
   // { name: 'notus', path: 'https://api.notus.team/openapi' },
-  { name: 'Machines API', path: 'https://docs.machines.dev/spec/openapi3.json' },
+  // { name: 'Machines API', path: 'https://docs.machines.dev/spec/openapi3.json' },
   { name: 'discriminator', path: './schemas/discriminator.yaml' },
   { name: 'bunq.com', path: './schemas/bunq.com.json', strict: false },
   { name: 'petStoreV3', path: 'https://petstore3.swagger.io/api/v3/openapi.json' },
