79d8ae4beae0a524678aa615ace1729a22577c1c
Ian VS <ian.vanschooten@gmail.com>
2025-12-10 09:18:47 -0500

    Add `allOf` schema example to test

--- packages/plugin-faker/src/generators/__snapshots__/getPets.ts
+++ packages/plugin-faker/src/generators/__snapshots__/getPets.ts
@@ -14,8 +14,8 @@ export function listPetsQueryParams(data?: Partial<ListPetsQueryParams>): ListPe
 /**
  * @description A paged array of pets
  */
-export function listPets200() {
-  return pets()
+export function listPets200(data?: Partial<ListPets200>): ListPets200 {
+  return Object.assign({}, pagination(), pets())
 }
 
 /**
