f356da7f0f35e61c57aa92ef70adbaa3a4e03acf
Stijn Van Hulle <stijn.vanhulle@outlook.com>
2025-12-22 23:52:15 +0100

    Update vitest.bench.config.ts

--- configs/vitest.bench.config.ts
+++ configs/vitest.bench.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    // Benchmark-specific configuration
     include: ['**/*.bench.ts'],
     exclude: ['**/node_modules/**', '**/dist/**', '**/mocks/**'],
     benchmark: {
