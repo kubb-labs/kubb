f7ddfc33a3c364744c975155cff28a022d612d30
Stijn Van Hulle <stijn@stijnvanhulle.be>
2026-01-08 18:14:41 +0000

    fix

--- packages/plugin-ts/src/factory.ts
+++ packages/plugin-ts/src/factory.ts
@@ -408,7 +408,7 @@ export function createEnumDeclaration({
    * - `literal`: literal union type
    * @default `'enum'`
    */
-  type?: 'enum' | 'asConst' | 'asPascalConst' | 'constEnum' | 'literal'
+  type?: 'enum' | 'asConst' | 'asPascalConst' | 'constEnum' | 'literal' | 'inlineLiteral'
   /**
    * Enum name in camelCase.
    */
@@ -419,7 +419,7 @@ export function createEnumDeclaration({
   typeName: string
   enums: [key: string | number, value: string | number | boolean][]
 }): [name: ts.Node | undefined, type: ts.Node] {
-  if (type === 'literal') {
+  if (type === 'literal' || type ==="inlineLiteral") {
     return [
       undefined,
       factory.createTypeAliasDeclaration(
