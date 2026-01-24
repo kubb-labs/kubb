1bbc520b18cf5cd3cec5dfe1567acad98e6ccb6d
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2026-01-24 20:17:12 +0800

    add example

---
+++ examples/zod/src/gen3/zod/index.ts
@@ -0,0 +1,13 @@
+export {
+  createItem200Schema,
+  createItemMutationRequestSchema,
+  createItemMutationResponseSchema,
+} from './createItemSchema.ts'
+export { itemSchema } from './itemSchema.ts'
+export { itemTypeASchema } from './itemTypeASchema.ts'
+export { itemTypeBSchema } from './itemTypeBSchema.ts'
+export {
+  updateItem200Schema,
+  updateItemMutationRequestSchema,
+  updateItemMutationResponseSchema,
+} from './updateItemSchema.ts'
