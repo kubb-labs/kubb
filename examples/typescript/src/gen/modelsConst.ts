1ed0ac52dc17834cbf030ef02d96de340c26a5d6
stijnvanhulle <5904681+stijnvanhulle@users.noreply.github.com>
2026-02-04 21:07:19 +0000

    chore(examples): update generated files

--- examples/typescript/src/gen/modelsConst.ts
+++ examples/typescript/src/gen/modelsConst.ts
@@ -247,6 +247,9 @@ export type Pet =
        * @type string
        */
       name: string
+      /**
+       * @type object | undefined
+       */
       category?: Category
       /**
        * @type array
@@ -275,6 +278,9 @@ export type Pet =
        * @type string
        */
       name: string
+      /**
+       * @type object | undefined
+       */
       category?: Category
       /**
        * @type array
--- examples/typescript/src/gen/modelsLiteral.ts
+++ examples/typescript/src/gen/modelsLiteral.ts
@@ -223,6 +223,9 @@ export type Pet =
        * @type string
        */
       name: string
+      /**
+       * @type object | undefined
+       */
       category?: Category
       /**
        * @type array
@@ -251,6 +254,9 @@ export type Pet =
        * @type string
        */
       name: string
+      /**
+       * @type object | undefined
+       */
       category?: Category
       /**
        * @type array
