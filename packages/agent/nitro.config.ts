820fc389c59bc2ea73b00a226d4b4452548b91d7
Stijn Van Hulle <stijn@stijnvanhulle.be>
2026-02-17 10:38:24 +0100

    cors

--- packages/agent/nitro.config.ts
+++ packages/agent/nitro.config.ts
@@ -3,4 +3,18 @@ export default defineNitroConfig({
   debug: false,
   serveStatic: false,
   compatibilityDate: '2026-02-17',
+  routeRules: {
+    '/api/**': {
+      cors: true,
+      prerender: true,
+      headers: {
+        // CORS headers
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': '*',
+        'Access-Control-Allow-Credentials': 'true',
+        'Access-Control-Allow-Headers': '*',
+        'Access-Control-Expose-Headers': '*',
+      },
+    },
+  },
 })
