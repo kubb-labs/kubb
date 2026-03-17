8e84f015ff1ceaac8727fa099d88c6cb486d5aaa
Stijn Van Hulle <stijn@stijnvanhulle.be>
2026-03-17 22:08:42 +0100

    naming

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -508,6 +508,6 @@ export type Logger<TOptions extends LoggerOptions = LoggerOptions> = {
 
 export type UserLogger<TOptions extends LoggerOptions = LoggerOptions> = Omit<Logger<TOptions>, 'logLevel'>
 
-export type { Generator } from './defineGenerator.ts'
+export type { CoreGeneratorV2, Generator, ReactGeneratorV2 } from './defineGenerator.ts'
 export type { DefineStorage } from './defineStorage.ts'
 export type { KubbEvents } from './Kubb.ts'
