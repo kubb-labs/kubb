8da1af5c3780c59bbb36d9eb3546e5d0f5da695d
Stijn Van Hulle <stijn@stijnvanhulle.be>
2026-03-31 20:33:01 +0200

    test cases

--- packages/plugin-ts/src/printers/printerTs.ts
+++ packages/plugin-ts/src/printers/printerTs.ts
@@ -146,7 +146,7 @@ export const printerTs = definePrinter<TsPrinter>((options) => {
 
         if (this.options.enumType === 'inlineLiteral' || !node.name) {
           const literalNodes = values
-            .filter(Boolean)
+            .filter((v): v is string | number | boolean => v !== null && v !== undefined)
             .map((value) => factory.constToTypeNode(value, typeof value as 'string' | 'number' | 'boolean'))
             .filter(Boolean)
 
