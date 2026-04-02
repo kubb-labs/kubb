b01164e537ee2a492476dcfa638cd228f232c602
Stijn Van Hulle <stijn@stijnvanhulle.be>
2026-04-02 17:32:04 +0200

    update for resolver

--- packages/plugin-client/src/resolvers/resolverClientLegacy.ts
+++ packages/plugin-client/src/resolvers/resolverClientLegacy.ts
@@ -1,7 +1,5 @@
 import { camelCase } from '@internals/utils'
-import type { RootNode } from '@kubb/ast/types'
-import type { ResolveBannerContext } from '@kubb/core'
-import { buildDefaultBanner, defineResolver } from '@kubb/core'
+import { defineResolver } from '@kubb/core'
 import type { PluginClient } from '../types.ts'
 
 /**
@@ -24,26 +22,5 @@ export const resolverClientLegacy = defineResolver<PluginClient>(() => ({
   },
   resolveName(name) {
     return this.default(name, 'function')
-  },
-  resolveBanner(node: RootNode | null, { output, config }: ResolveBannerContext) {
-    let defaultBanner = ''
-    if (config?.output?.defaultBanner !== false) {
-      defaultBanner = buildDefaultBanner({
-        title: node?.meta?.title,
-        description: node?.meta?.description,
-        version: node?.meta?.version,
-        config,
-      })
-    }
-
-    if (!output?.banner) {
-      return defaultBanner || undefined
-    }
-
-    if (typeof output.banner === 'function') {
-      return node ? `${output.banner(node)}\n${defaultBanner}` : defaultBanner || undefined
-    }
-
-    return `${output.banner}\n${defaultBanner}`
   },
 }))
