6c4c123b131e1a8e9ffdf3fcb535bca461c25d58
stijnvanhulle <5904681+stijnvanhulle@users.noreply.github.com>
2026-05-18 19:08:26 +0000

    chore: format and lint fixes

--- packages/renderer-jsx/src/SyncRuntime.tsx
+++ packages/renderer-jsx/src/SyncRuntime.tsx
@@ -12,17 +12,7 @@ import {
   createType,
 } from '@kubb/ast'
 import React from 'react'
-import {
-  KUBB_ARROW_FUNCTION,
-  KUBB_CONST,
-  KUBB_EXPORT,
-  KUBB_FILE,
-  KUBB_FUNCTION,
-  KUBB_IMPORT,
-  KUBB_JSX,
-  KUBB_SOURCE,
-  KUBB_TYPE,
-} from './constants.ts'
+import { KUBB_ARROW_FUNCTION, KUBB_CONST, KUBB_EXPORT, KUBB_FILE, KUBB_FUNCTION, KUBB_IMPORT, KUBB_JSX, KUBB_SOURCE, KUBB_TYPE } from './constants.ts'
 import type { KubbReactElement } from './types.ts'
 
 type OnText = (text: string) => void
@@ -75,7 +65,9 @@ function collectCodeNodes(props: Record<string, unknown>): CodeNode[] {
 function collectCode(element: unknown, nodes: CodeNode[]): void {
   walkElement(
     element,
-    (text) => { if (text.trim()) nodes.push(createText(text)) },
+    (text) => {
+      if (text.trim()) nodes.push(createText(text))
+    },
     (type, props) => resolveCodeNode(type, props, nodes),
   )
 }
@@ -88,7 +80,13 @@ function resolveCodeNode(type: string, props: Record<string, unknown>, nodes: Co
 
   if (type === KUBB_JSX) {
     let value = ''
-    walkElement(props['children'], (t) => { value += t }, () => {})
+    walkElement(
+      props['children'],
+      (t) => {
+        value += t
+      },
+      () => {},
+    )
     if (value) nodes.push(createJsx(value))
     return
   }
