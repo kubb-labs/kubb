0d535314f2970ae60b5c0651b693e2f30d562cd2
Stijn Van Hulle <stijn.vanhulle@outlook.com>
2026-09-24 00:27:36 +0200

    feat(ast): add isBareRef helper

--- packages/ast/src/utils/index.ts
+++ packages/ast/src/utils/index.ts
@@ -1,4 +1,4 @@
 export { extractStringsFromNodes } from './extractStringsFromNodes.ts'
-export { resolveRefName } from './refs.ts'
+export { isBareRef, resolveRefName } from './refs.ts'
 export { collectImportedRefNames, collectUsedSchemaNames, findCircularSchemas, findCircularSchemasFromGraph } from './schemaGraph.ts'
 export { getSchemaLiteralValues, inferDiscriminatorPropertyName, resolveSchemaProperties } from './schemaProperties.ts'
