07d987b85b7b80397927b2c51cb55934109cde01
Stijn Van Hulle <stijn.vanhulle@outlook.com>
2026-09-24 00:34:23 +0200

    fix(kit): expose isBareRef through ast namespace only

--- packages/kit/src/index.ts
+++ packages/kit/src/index.ts
@@ -1,4 +1,4 @@
-export { ast, isBareRef } from '@kubb/ast'
+export { ast } from '@kubb/ast'
 export { Url } from './Url.ts'
 export { macroDiscriminatorEnum, macroEnumName, macroRenameSchema, macroSimplifyUnion } from './macros/index.ts'
 export { childName, containsCircularRef, enumPropName, extractRefName, isStringType, mergeAdjacentObjectsLazy, syncSchemaRef } from './utils/index.ts'
